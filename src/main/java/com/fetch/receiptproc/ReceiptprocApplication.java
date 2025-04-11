523ba2e07d2f059d955777e8ecb1c65dfc754d88
Gurbeer <gurbeersinghsokhi22@gmail.com>
2025-04-11 10:41:23 -0400

    first commit

---
+++ src/main/java/com/fetch/receiptproc/ReceiptprocApplication.java
@@ -0,0 +1,13 @@
+package com.fetch.receiptproc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptprocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptprocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fetch/receiptproc/ReceiptprocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetch.receiptproc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptprocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
